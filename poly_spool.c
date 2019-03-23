5c70d81065e36960851fa87fa7bcb02b922a6ed8
Jason Berger <berge472@gmail.com>
2019-03-22 23:55:34 -0400

    adding in spool code

---
+++ poly_spool.c
@@ -0,0 +1,239 @@
+/**
+  *@file poly_spool.c
+  *@brief ack/retry spool for poly packet
+  *@author Jason Berger
+  *@date 03/22/2019
+  */
+
+#include "poly_spool.h"
+#include <assert.h>
+
+
+void poly_spool_init(poly_spool_t* spool, int len)
+{
+  SPOOL_LOCK;
+
+  //set default values
+  spool->mMaxEntries = len;
+  spool->mEntries = (spool_entry_t*)malloc(sizeof(spool_entry_t) * len);
+  spool->mCount = 0;
+  spool->mReadyCount =0;
+  spool->mTimeOut = 400; //Doherty Threshold?
+  spool->mMaxRetries = 10;
+  spool->mLock  = false;
+  spool->mSuccessfulMessages =0;
+  spool->mFailedMessages =0;
+
+  //set all entries to free
+  for(int i=0; i < len; i++)
+  {
+    spool->mEntries[i].mState = ENTRY_STATE_FREE;
+  }
+
+  SPOOL_UNLOCK;
+}
+
+/**
+  *@brief gets next entry in spool with mState == state
+  *@param spool - pointer to spool
+  *@param state - state to look for
+  *@return index of entry if found
+  *@return -1 if none are found
+  */
+static inline int poly_spool_get_next(poly_spool_t* spool, spool_entry_state_e state)
+{
+  int idx = -1;
+  for(int i=0; i < spool->mMaxEntries; i++)
+  {
+    if(spool->mEntries[i].mState == state)
+    {
+      idx =i;
+      break;
+    }
+  }
+
+  return idx;
+}
+
+
+void poly_spool_deinit(poly_spool_t* spool)
+{
+  SPOOL_LOCK;
+  spool->mMaxEntries = 0;
+  spool->mReadyCount =0;
+  spool->mCount =0;
+  free(spool->mEntries);
+  SPOOL_UNLOCK;
+}
+
+
+spool_status_e poly_spool_push(poly_spool_t* spool, poly_packet_t* packet, spool_entry_ack_type ackType , spool_ack_cb cb )
+{
+  SPOOL_LOCK;
+  spool_status_e status;
+
+  //get next free Entry
+  int idx = poly_spool_get_next(spool, ENTRY_STATE_FREE);
+  spool_entry_t* entry;
+
+  //make sure we arent full
+  if(spool->mCount >= spool->mMaxEntries)
+  {
+    SPOOL_UNLOCK;
+    return SPOOL_OVERFLOW;
+  }
+
+  if(idx > ENTRY_NONE)
+  {
+    //reset entry properties
+    entry = &spool->mEntries[idx];
+    entry->mTimeOut = 0;
+    entry->mState = SPOOL_STATE_READY;
+    entry->mAttempts = 0;
+    entry->mAckType = ackType;
+    entry->mAckToken = token;
+    entry->f_ackCallback = cb;
+
+    //copy in packet data
+    memcpy((void*)entry->mPacket, (void*)packet, sizeof(poly_packet_t));
+
+    //increment counts
+    spool->mCount++;
+    spool->mReadyCount++;  //decrement ready count
+  }
+
+  SPOOL_UNLOCK;
+  return SPOOL_OK;
+}
+
+
+spool_status_e poly_spool_pop(poly_spool_t* spool, poly_packet_t* packet)
+{
+  SPOOL_LOCK;
+  spool_status_e status;
+
+  //get next ready packet
+  int idx = poly_spool_get_next(spool, ENTRY_STATE_READY);
+  spool_entry_t* entry;
+  uint16_t newToken =0;
+
+  if(idx > ENTRY_NONE)
+  {
+    entry = spool->mEntries[idx];
+
+
+    if(entry->mAckType == ACK_TYPE_TOKEN)
+    {
+      //if ack type is token, then we generate a randome token and make sure it wont be 0 without the ack flag
+      while(newToken ==0)
+      {
+        newToken = rand() & SPOOL_TOKEN_MASK;
+      }
+
+      //set the token with the ack request flag so the receiver knows to ack
+      entry->mPacket.mHeader.mToken = newToken | SPOOL_TOKEN_ACK_REQ;
+
+      //reset the timeout counter to the spools timeout setting
+      entry->mTimeOut = spool->mTimeOut;
+
+      entry->mState = ENTRY_STATE_WAITING;
+      entry->mAttempts++;
+    }
+    else
+    {
+      //If we dont need to an ack, then we are done with this packet
+      entry->mState = ENTRY_STATE_FREE;
+      spool->mCount--;
+    }
+
+    spool->mReadyCount--;
+
+    //copy the packet out
+    memcpy((void*)packet, (void*)entry->mPacket, sizeof(poly_packet_t) );
+    status = SPOOL_OK;
+
+  }
+  else
+  {
+    status = SPOOL_UNDERFLOW
+  }
+
+  SPOOL_UNLOCK;
+  return status;
+}
+
+
+bool poly_spool_ack(poly_spool_t* spool, poly_packet_t* response)
+{
+  bool match = false;
+  int i;
+  //if the token is 0 or is an ack request, we ignore it
+  if((ack_token == 0) || (ack_token & SPOOL_TOKEN_ACK_REQ))
+  {
+    return false;
+  }
+
+  SPOOL_LOCK;
+
+  //Check each entry in the state ENTRY_STATE_WAITING to see if the token matches
+  for( i=0;i < spool->mMaxEntries; i++)
+  {
+    //if entry is waiting, and the token matches after masking off the request bit
+    if((spool->mEntries[i].mState == ENTRY_STATE_WAITING) && (ackToken == (spool->mEntries[i].mPacket.mHeader.mToken & SPOOL_TOKEN_MASK )))
+    {
+      //match found, free entry
+      spool->mEntries[i].mState = ENTRY_STATE_FREE;
+      spool->mCount--;
+
+      match = true;
+      break;
+    }
+  }
+
+  SPOOL_UNLOCK;
+
+  //if we found a match and the entry has a callback, call it
+  if(match && (spool->mEntries[i].f_ackCallback != NULL))
+  {
+    spool->mEntries[i].f_ackCallback(response);
+  }
+
+  return match;
+}
+
+
+void poly_spool_tick(poly_spool_t* spool, int ms)
+{
+  SPOOL_LOCK;
+  spool_entry_t* entry;
+
+  for(int i=0; i < spool->mSize; i++)
+  {
+    entry = &c->mEntries[i];
+
+    //time only matters to waiting entries
+    if(entry->mState == ENTRY_STATE_WAITING)
+    {
+      //subtract the time from the entries
+      entry->mTimeOut -= ms;
+
+      //If we go to zero, the packet has timed out
+      if(entry->mTimeOut <= 0)
+      {
+        //if we havent maxxed out retries set it to ready and try again
+        if(entry->mAttempts < spool->mMaxRetries)
+        {
+          entry->mState = ENTRY_STATE_READY;
+          spool->mReadyCount++;
+        }
+        else //if we max out our retries, free the slot and note a failed message
+        {
+          entry->mState = ENTRY_STATE_FREE;
+          spool->mCount--;
+          spool->mFailedMessages++;
+        }
+      }
+    }
+  }
+  SPOOL_UNLOCK;
+}
---
+++ poly_spool.h
@@ -0,0 +1,114 @@
+/**
+  *@file poly_spool.h
+  *@brief ack/retry spool for poly packet
+  *@author Jason Berger
+  *@date 03/22/2019
+  */
+
+#pragma once
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "poly_packet.h"
+
+typedef void (*spool_ack_cb)(poly_packet_t* response);
+
+#define ENTRY_NONE -1
+
+#define SPOOL_TOKEN_MASK 0x7FFF
+#define SPOOL_TOKEN_ACK_REQ 0x8000
+
+//State of entries in the spool
+typedef enum{
+  ENTRY_STATE_FREE,     //Entry is unused
+  ENTRY_STATE_READY,    //Entry is ready to send
+  ENTRY_STATE_WAITING   //Entry was sent and is waiting for an ack or to timeout for a retry
+} spool_entry_state_e;
+
+//Spool entry ack type
+typedef enum{
+   ACK_TYPE_NONE,         //no ack needed
+   ACK_TYPE_TOKEN,        //request ack via token
+   ACK_TYPE_PASSTHROUGH  //pass through message, dont touch the token (used when relaying messages so the endpoints can handle ack/retry)
+} spool_entry_ack_type;
+
+//Status of spool actions
+typedef enum{
+  SPOOL_OK = 0,           //Everything is ok
+  SPOOL_OVERFLOW ,     //Spool is full
+  SPOOL_UNDERFLOW ,    //Spool is empty
+  SPOOL_ERROR         //Misc errors
+} spool_status_e;
+
+
+typedef struct{
+    spool_entry_state_e mState;     //current state of entry
+    int mTimeOut;                   //current state of timeout counter
+    int mAttempts;                  //number of attempts to send this packet so far
+    uint8_t mAckType;               //type of ack requested for this packet
+    poly_packet_t mPacket;          //actual packet being sent
+    spool_ack_cb f_ackCallback;     //callback function for when packet is acknowledged
+  } poly_spool_entry_t;
+
+  typedef struct{
+    spool_entry_t* mEntries;        //entries in spool
+    int mMaxEntries;                //max number of entries in spool
+    int mCount;                     //current number of entries in spool
+    int mReadyCount;                //number of entries in the READY state
+    int mTimeOut;                   //timeout for entries
+    int mMaxRetries;                //maximum attempts for each packet before giving up
+    uint8_t mLock;                  //lock bit/mutex //TODO add mutex support when available
+    //used for debugging
+    int mSuccessfulMessages;        //successful messages
+    int mFailedMessages;            //failed
+  }poly_spool_t;
+
+/**
+  *@brief initialize a spool
+  *@param spool ptr to spool to initialize
+  *@param len number of entries to keep in spool
+  */
+void poly_spool_init(poly_spool_t* spool, int len);
+
+/**
+  *@brief deinit the spool
+  *@param spool spool to deinit
+  */
+void poly_spool_deinit(poly_spool_t* spool);
+
+/**
+  *@brief pushes a new packet onto the spool
+  *@param spool ptr to spool
+  *@param packet packet to add to spool
+  *@param ackType type of ack for this packet (ACK_TYPE_TOKEN, ACK_TYPE_NONE, ACK_TYPE_PASSTHROUGH)
+  *@param cb callback to fire when ack is received (NULL for no callback)
+  *@return SPOOL_OK push was successful
+  *@return SPOOL_OVERFLOW spool was already full
+  */
+spool_status_e poly_spool_push(poly_spool_t* spool, poly_packet_t* packet, spool_entry_ack_type ackType, spool_ack_cb cb );
+
+/**
+  *@brief pulls
+  *@param spool ptr to spool
+  *@param packet packet to add to spool
+  *@return SPOOL_OK push was successful
+  *@return SPOOL_UNDERFLOW spool is empty or no packets are in the READY state
+  */
+spool_status_e poly_spool_pop(poly_spool_t* spool, poly_packet_t* packet);
+
+/**
+  *@brief passes an ack token to the spool to check for matches
+  *@param spool ptr to spool
+  *@param ackToken token to register. If it matches any waiting entries, they will be freed
+  *@return true if matching request token was found
+  *@return false if no match is found
+  */
+bool poly_spool_ack(poly_spool_t* spool, SPOOL_TOKEN_TYPE ackToken);
+
+/**
+  *@brief increments the time for the spool so it can check timeouts
+  *@param spool ptr to spool
+  *@param ms time in milliseconds since the last update
+  */
+void poly_spool_tick(poly_spool_t* spool, int ms);
